c5576d13e1ea590cccb8fd6c06a26f1d0fe54a63
Abdulrahman Afaraetu <afaraeturahman@gmail.com>
2021-11-07 09:21:03 +0100

    delete

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ function createNote(noteTitle, noteBody) {
     let h2 = document.createElement("h2")
     let p = document.createElement("p")
     let xButton = document.createElement("Button")
-    let eButton = document.createElement("Button")
+    // let eButton = document.createElement("Button")
 
     xButton.classList.add("delete")
-    eButton.classList.add("edit")
+    // eButton.classList.add("edit")
 
     // creating text node
     let xText = document.createTextNode("X")
@@ -29,13 +29,13 @@ function createNote(noteTitle, noteBody) {
     h2.appendChild(h2TN)
     p.appendChild(pTN)
     xButton.appendChild(xText)
-    eButton.appendChild(eText)
+    // eButton.appendChild(eText)
 
     // adding children to the a tag
     a.appendChild(h2)
     a.appendChild(xButton);
     a.appendChild(p);
-    a.appendChild(eButton)
+    // a.appendChild(eButton)
     // setting attribute
     a.setAttribute("href", "#")
 
@@ -72,27 +72,30 @@ function removeItem(e) {
    
     if(e.target.classList.contains("delete")){//check if the target contains the class name "delete"
         if(confirm("Are you sure you want to remove this note")){
-            let li = e.target.parentElement.parentElement
-            // grab the items
-            let ul = document.getElementById("notes")
-            ul.removeChild(li)
+          let li = e.target.parentElement.parentElement
+          // grab the items
+          let ul = document.getElementById("notes")
+          ul.removeChild(li)
+          //remove the above and try local storage direct
         }
-    }
-
-    count -= 1
+      }
+     
+        count -= 1
+         window.localStorage.setItem("count", count);
+          createNote(noteTitle, noteBody);
 
      if (count < 1) {
        document.getElementById("no-notes").className = "";
      }
 }
 
-function edit(e) {
-  if(e.target.classList.contains("edit")){
-    let li = e.target.parentElement.parentElement
-    let ul = document.getElementById("notes")
+// function edit(e) {
+//   if(e.target.classList.contains("edit")){
+//     let li = e.target.parentElement.parentElement
+//     let ul = document.getElementById("notes")
     
-  }
-}
+//   }
+// }
 
 for (i=0; i<count + 1; i++){
   // instead of getItem method, key method is used to
